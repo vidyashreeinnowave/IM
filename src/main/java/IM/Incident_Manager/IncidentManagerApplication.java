a61fb02a82f753ca48d36c26bc4220720c58c988
Vidyashree <vidyashree.c@innowave.tech>
2025-11-14 12:31:17 +0530

    initial commit

---
+++ src/main/java/IM/Incident_Manager/IncidentManagerApplication.java
@@ -0,0 +1,13 @@
+package IM.Incident_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IM/Incident_Manager/IncidentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package IM.Incident_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
